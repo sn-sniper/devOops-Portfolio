0fcab62e66669b54ccbfc9594edb2cf8a3c278a5
sn-sniper <assaadhassan00021@gmail.com>
2025-04-05 19:33:36 +0300

    s

---
+++ src/components/ui/Select-Dropdown/SelectDropdown.jsx
@@ -0,0 +1,61 @@
+import { useState, useRef, useEffect } from "react";
+import { FaChevronDown } from "react-icons/fa6";
+import { Figma } from "grommet-icons";
+import { Template } from "grommet-icons";
+import { GooglePlay } from "grommet-icons";
+import { Desktop } from "grommet-icons";
+import { Help } from "grommet-icons";
+import "./SelectDropdown.css";
+
+
+const options = [
+  { label: "UI UX", value: "uiux", icon: <Figma color="plain" /> },
+  { label: "Website", value: "website", icon: <Template color="#636edf" /> },
+  { label: "Mobile App", value: "mobile", icon: <GooglePlay color="plain" /> },
+  { label: "Software", value: "software", icon: <Desktop color="#1F7D53" /> },
+  { label: "Other", value: "other", icon: <Help color="red" /> },
+];
+
+export default function SelectDropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const dropdownRef = useRef(null);
+
+  const handleOptionClick = (option) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <div className="custom-dropdown" ref={dropdownRef}>
+      <div className="dropdown-header" onClick={() => setIsOpen(!isOpen)}>
+        <span>{selected ? selected.label : "Select a platform"}</span>
+        <FaChevronDown className={`chevron ${isOpen ? "open" : ""}`} />
+      </div>
+      {isOpen && (
+        <div className="dropdown-list">
+          {options.map((option) => (
+            <div
+              key={option.value}
+              className="dropdown-option"
+              onClick={() => handleOptionClick(option)}
+            >
+              <span className="icon">{option.icon}</span>
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,15 +1,10 @@
 import "@styles/Testing.css";
-import HContact from "@components/HContact";
-import Modal from "@components/ui/Modal/Modal";
-import ServiceReqForm from "@components/ui/Forms/ServiceReq";
+import SelectDropdown from "@components/ui/Select-Dropdown/SelectDropdown";
 
 function Testing() {
   return (
     <div className="Testing-Container">
-      <HContact />
-      <Modal header="Submit Service Request" isOpen={true} onClose={() => {}}>
-        <ServiceReqForm />
-      </Modal>
+      <SelectDropdown />
     </div>
   );
 }
